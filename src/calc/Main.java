cdeb8166a4cb999bb776eca2cd89d6bd3ff77c51
rnwkgus2@gmail.com <rnwkgus2@gmail.com>
2020-10-21 13:09:07 +0900

    첫번째 업로드

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -1,5 +1,15 @@
 package calc;
+import java.io.*;
 
 public class Main {
-
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String Question = br.readLine();
+		String[] arr = new String[Question.length()];
+		for(int i=0; i<Question.length(); i++) {
+			arr[i] = 
+		}
+		
+	}
 }
