db7c878932aade48a56dd8062682a57496fa2773
rnwkgus2@gmail.com <rnwkgus2@gmail.com>
2020-10-21 14:07:15 +0900

    MK1 순서를 구분하지 않는 사칙연산

--- src/calc/Main.java
+++ src/calc/Main.java
@@ -1,15 +1,18 @@
 package calc;
 import java.io.*;
-
 public class Main {
 	public static void main(String args[]) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String Question = br.readLine();
-		String[] arr = new String[Question.length()];
-		for(int i=0; i<Question.length(); i++) {
-			arr[i] = 
-		}
+		System.out.println("계산할 값을 입력하세요.");
+		String[] Question = br.readLine().split(" ");
 		
+		int i = 1;
+		int first = Integer.parseInt(Question[0]);
+		int result = first;
+		while(i<Question.length) {
+			result = Calc.calculate(result, Question[i], Integer.parseInt(Question[i+1]));
+			i+=2;
+		}
+		System.out.println(result);
 	}
 }
